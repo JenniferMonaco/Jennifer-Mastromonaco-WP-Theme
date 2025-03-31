f138e3bed0b52406931cacad88dfcd7cf0009d22
Jenn <6235609@johnabbottcollege.net>
2025-03-31 09:21:00 -0400

    initialcommit

--- index.php
+++ index.php
@@ -35,3 +35,33 @@
 
   <?php wp_footer(); ?>
   <?php // this function loads the footer.php file ?>
+
+  <!-- START STEP 2.8 ADD H1 AND WORDPRESS LOOP -->
+  <h1><?php the_title(); // AUTOMATICALLY ADDS THE PAGE TITLE ?></h1>
+  
+  <!-- opening flex-container will go here -->
+  
+  <main id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+  <?php // NOTE IDs AND CLASSES ARE AUTOMATICALLY ADDED HERE ?>
+
+
+  <!-- START WORDPRESS LOOP -->
+
+      <?php 
+          if ( have_posts() ) : 
+          while ( have_posts() ) : the_post();
+              the_content();
+          endwhile;
+      else :
+          _e( 'Sorry, no posts matched your criteria.', 'billy-poppins' );
+          // MAKE SURE TO CHANGE BILLY-POPPINS HERE
+      endif;
+      ?>
+
+      <?php wp_link_pages(); ?>
+
+  <!-- END WORDPRESS LOOP -->    
+  </main>
+
+   <!-- END STEP 2.8 ADD H1 AND WORDPRESS LOOP -->
+  <!-- closing flex-container will go here -->
